fa86e9499eea668e7704a9a5118ba978cceb30ab
RdhYuGaan <radithyugan22@gmail.com>
2024-07-19 10:39:58 +0530

    Backery Web Site

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let navbar = document.querySelector('.header .navbar');
+let menu = document.querySelector('#menu-btn');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let cart = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.add('active');
+}
+
+document.querySelector('#close-form').onclick = () =>{
+    cart.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    cnteredSlides:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
